19a9384a07ca96dea6c386275e51d0db2b28752c
kuarisu <kuarisuran@gmail.com>
2016-04-28 16:41:47 +0200

    Add room

---
+++ Assets/Script/Camera/Camera_LookAt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_LookAt : MonoBehaviour {
+
+    public Transform target;
+
+	
+	void Start () {
+
+	}
+	
+
+	void LateUpdate () {
+
+        if (target != null)
+        {
+            //transform.LookAt(target.position);
+        }
+
+    }
+}
---
+++ Assets/Script/Character/Character_Movement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Movement : MonoBehaviour {
+
+    private bool IsMoving = false;      //Définit si le Player est en mouvement ou non
+    private float Movement = 0.15f;      //Vitesse de déplacement du Player
+
+    // Use this for initialization
+    void Start () {
+
+        this.transform.position = new Vector3(0, 1, 0);
+	}
+
+    void Update()
+    {
+        if (Input.GetKey("z")) //&& IsMoving == false
+        {
+            StartCoroutine(UpMovement());
+        }
+        if (Input.GetKey("s") )
+        {
+            StartCoroutine(BackMovement());
+        }
+        if (Input.GetKey("d") )
+        {
+            StartCoroutine(RightMovement());
+        }
+        if (Input.GetKey("q") )
+        {
+            StartCoroutine(LeftMovement());
+        }
+
+    }
+
+    IEnumerator UpMovement()
+    {
+        IsMoving = true;
+        transform.position = (transform.position + (transform.forward * Movement));
+        yield return null;
+        IsMoving = false;
+    }
+    IEnumerator BackMovement()
+    {
+        IsMoving = true;
+        transform.position = transform.position - (transform.forward * Movement);
+        yield return null;
+        IsMoving = false;
+    }
+    IEnumerator RightMovement()
+    {
+        IsMoving = true;
+        transform.position = transform.position + (transform.right * Movement);
+        yield return null;
+        IsMoving = false;
+    }
+
+    IEnumerator LeftMovement()
+    {
+        IsMoving = true;
+        transform.position = transform.position - (transform.right * Movement);
+        yield return null;
+        IsMoving = false;
+    }
+
+}
+
+
